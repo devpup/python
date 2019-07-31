f9b6a799e1a1f9ac131cdde6640ebebe717e6404
devpup <whdgh777@naver.com>
2019-07-31 17:35:34 +0900

    first commit

---
+++ hello/test.py
@@ -0,0 +1,26 @@
+print(4 // 2)
+print(4 / 2)
+print(type(4//2))
+print(type(4/2))
+
+c = 'c'
+print(type(c))
+
+str = 'abc'
+print(str[0])
+
+message = "Hello World"
+print(message[0:7])
+print(message[:7])
+print(message[:len(message)])
+
+message = "aaa bbb ccc"
+print(message.split(' '))
+print(message.split(' ')[1])
+a,b,c = message.split(' ')
+print(a + b + c)
+
+x,y,z=1,2,3
+print(x)
+print(y)
+print(z)
